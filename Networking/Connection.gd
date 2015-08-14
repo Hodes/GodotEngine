42442d87edf41b2494fa4e83377912244110b86e
horbtec <henrique.otavio.es@gmail.com>
2015-08-14 18:44:05 -0300

    A project focusing on techniques to develop for godot with networking. On this commit just TCP support.

---
+++ Networking/Connection.gd
@@ -0,0 +1,243 @@
+##
+# Author: HODES 
+#        (Henrique Otavio do Espirito Santo Becker)
+#
+# A Class to help on TCP|UDP connection within Godot Game Engine
+# Credits on exelent reserarch resources, on how to handle 
+# Networking related in GDScript: 
+#
+# Kermer                         - https://github.com/Kermer/Godot/wiki
+#
+# Theo apud. Kermer              - http://www.godotengine.org/forum/viewtopic.php?f=14&t=565
+#
+# jrimclean                      - https://github.com/jrimclean
+#                                  https://github.com/jrimclean/godot-state-sync-demo
+#                                  https://github.com/jrimclean/godot-snapshot-interpolation-demo
+#
+# GafferOnGames apud. jrimclean  - http://gafferongames.com/
+#
+
+extends Node
+
+class Client:
+	var tcpConnection = null
+	var tcpStream = null
+	var udpPacketPeer = null
+	var ip = ""
+	var data = null
+	# Just to check if is host uppon data receiving
+	var isHost = false
+
+class Server:
+	var tcpServer = null
+	var udpPacketPeer = null
+	var clients = []
+
+var serverObject = null
+var clientObject = null
+var isHost = false
+var isConnecting = false
+var isReady = false
+
+export(String) var ip = "127.0.0.1"
+export(int) var tcpPort = 49652
+export(int) var udpPort = 49650
+export(bool) var enableUDP = true
+
+func setIP(ip):
+	self.ip = ip
+func setTCPPort(port):
+	self.tcpPort = int(port)
+func setUDPPort(port):
+	self.udpPort = int(port)
+	
+func _init(ip, tcpPort = null, udpPort = null):
+	self.ip = ip
+	if tcpPort != null:
+		self.tcpPort = int(tcpPort)
+	if udpPort != null:
+		self.udpPort = int(udpPort)
+	#As HOST Signals
+	self.add_user_signal("onClientConnect",[
+		{"name":"oClient", "type":TYPE_OBJECT}
+		])
+	self.add_user_signal("onClientDisconnect",[
+		{"name":"oClient", "type":TYPE_OBJECT}
+		])
+	#As Client Signals
+	self.add_user_signal("onConnect", [
+		{"name":"ip", "type":TYPE_STRING},
+		{"name":"port", "type":TYPE_INT}
+		])
+	self.add_user_signal("onDisconnect", [
+		{"name":"ip", "type":TYPE_STRING},
+		{"name":"port", "type":TYPE_INT}
+		])
+	#As HOST and Client 
+	self.add_user_signal("onReceive", [
+		{"name":"data", "type":TYPE_OBJECT},
+		{"name":"sender", "type":TYPE_OBJECT}
+		])
+
+func _process( delta ):
+	#As HOST handling
+	if self.isHost and self.isReady:
+		#Handle client connections
+		if serverObject.tcpServer.is_connection_available(): # check if someone's trying to connect
+			var oClient = Client.new()
+			oClient.tcpConnection = serverObject.tcpServer.take_connection() # accept connection
+			oClient.tcpStream = PacketPeerStream.new() # make new data transfer object for him
+			oClient.tcpStream.set_stream_peer( oClient.tcpConnection )
+			oClient.ip = oClient.tcpConnection.get_connected_host() 
+			print("Client connected ",oClient.ip)
+			serverObject.clients.append( oClient ) # we need to store him somewhere, that's why we created our Array
+			#Emit the signal
+			self.emit_signal("onClientConnect", oClient)
+		#For each client perform some action
+		for client in serverObject.clients:
+			if !client.tcpConnection.is_connected(): # NOT connected
+				print("Client disconnected")
+				var index = serverObject.clients.find( client )
+				#Emit the signal
+				self.emit_signal("onClientDisconnect", client)
+				serverObject.clients.remove( index )
+			else:
+				var available = client.tcpStream.get_available_packet_count()
+				if available > 0:
+					for i in range(available):
+						dataReceived(client.tcpStream.get_var(), client)
+	#As Client Handling
+	elif(not self.isHost):
+		if not self.isReady: # it's inside _process, so if last status was STATUS_CONNECTING
+			if clientObject.tcpConnection.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+				_onClientReady(ip, self.tcpPort)
+				return # skipping this _process run
+		if (clientObject.tcpConnection.get_status() == StreamPeerTCP.STATUS_NONE 
+			or clientObject.tcpConnection.get_status() == StreamPeerTCP.STATUS_ERROR):
+			print( "Server disconnected? " )
+			_onDisconnectAsClient(ip,self.tcpPort)
+			return # skip the process because its disconnected
+		#Process the server received data
+		if self.isReady:
+			var available = clientObject.tcpStream.get_available_packet_count()
+			if available > 0:
+				for i in range(available):
+					#Send without a sender, indicating it has came from server
+					dataReceived(clientObject.tcpStream.get_var())
+
+func clearStates():
+	self.isHost = false
+	self.isReady = false
+	self.isConnecting = false
+
+func disconnect():
+	if(self.isHost and serverObject != null):
+		serverObject.clients.clear()
+		serverObject.udpPacketPeer.close()
+		serverObject.tcpServer.stop()
+		print("Server Disconnected")
+	elif(not self.isHost and clientObject != null):
+		if(clientObject.tcpConnection.is_connected()):
+			clientObject.tcpConnection.disconnect()
+		if(clientObject.udpPacketPeer != null):
+			clientObject.udpPacketPeer.close()
+		print("Client connection closed")
+	clearStates()
+	set_process( false )
+
+func start_server():
+	if(self.isReady):
+		print("Must to disconnect first, in order to start a new server connection.")
+		return false
+	print("Starting Server")
+	#Start new instance for server connection
+	#TCP will be used to mantain the connection active
+	#also for some reliable operations
+	serverObject = Server.new()
+	serverObject.tcpServer = TCP_Server.new()
+	serverObject.udpPacketPeer = PacketPeerUDP.new()
+	#Try to listen for connections
+	if serverObject.tcpServer.listen( self.tcpPort ) == 0:
+		print("Server started listening on port ", self.tcpPort )
+	else:
+		print("Failed to start server on port ", self.tcpPort)
+		return false
+	#Start UDP
+	if (serverObject.udpPacketPeer.listen(self.udpPort) != OK):
+		print("Error start listening UDP on port ", self.udpPort)
+		return false
+	else:
+		print("Listening UDP on port ", self.udpPort)
+		self.isHost = true
+		self.isReady = true
+	set_process( true )
+	#Server started success
+	return true
+
+func start_client():
+	if(self.isReady or self.isConnecting):
+		print("Must to disconnect first, in order to start a new client connection.")
+		return false
+	print("Starting as client connection")
+	#Create client object
+	clientObject = Client.new()
+	clientObject.tcpConnection = StreamPeerTCP.new()
+	# Starts the connection
+	clientObject.tcpConnection.connect( ip, self.tcpPort )
+	# Create the packet stream
+	clientObject.tcpStream = PacketPeerStream.new()
+	clientObject.tcpStream.set_stream_peer( clientObject.tcpConnection )
+	# since connection is created from StreamPeerTCP it also inherits its constants
+	# get_status() returns following (int 0-3) values:
+	if clientObject.tcpConnection.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+		_onClientReady(ip, self.tcpPort)
+		return true
+	elif clientObject.tcpConnection.get_status() == StreamPeerTCP.STATUS_CONNECTING:
+		print( "Trying to connect ",ip," :",self.tcpPort)
+	elif (clientObject.tcpConnection.get_status() == StreamPeerTCP.STATUS_NONE 
+		or clientObject.tcpConnection.get_status() == StreamPeerTCP.STATUS_ERROR):
+		print( "Couldn't connect to "+ip+" :",self.tcpPort)
+		_onDisconnectAsClient(ip, self.tcpPort)
+		return false
+	set_process(true) # start processing so it can check if connected success
+	isConnecting = true
+	return true
+
+func _onClientReady(ip, port):
+	print( "Connected to ",ip," :", port)
+	isReady = true
+	isConnecting = false
+	self.emit_signal("onConnect", ip, port)
+	set_process(true) # start processing if connected
+
+func _onDisconnectAsClient(ip, port):
+	self.emit_signal("onDisconnect", ip, port)
+	self.disconnect()
+
+#Send data no matter if is Host or client, will decide automatically
+func sendData(data, to=null):
+	if(not self.isReady):
+		print("A connection must be ready in order to start sending data")
+		return false;
+	if (self.isHost):
+		_sendDataAsHost(data, to)
+	else:
+		_sendDataAsClient(data)
+	return true
+
+# This will send thru UDP protocoll usually 
+func sendImmediateData(data, to=null):
+	pass
+
+func _sendDataAsHost(data, to=null):
+	if(to!=null):
+		to.tcpStream.put_var(data)
+	else:
+		for oClient in serverObject.clients:
+			oClient.tcpStream.put_var(data)
+
+func _sendDataAsClient(data):
+	clientObject.tcpStream.put_var(data)
+
+func dataReceived(data, from={"isHost": true}):
+	self.emit_signal("onReceive", data, from)
---
+++ Networking/Program.gd
@@ -0,0 +1,100 @@
+extends Node
+
+var Connection = preload("res://Connection.gd")
+
+var oConnection = null
+
+var txIP = null
+var txPort = null
+var txUDPPort = null
+var btnStartServer = null
+var btnStartClient = null
+var btnDisconnect = null
+var output = null
+var input = null
+
+func _ready():
+	txIP = get_node("txIP")
+	txPort = get_node("txPort")
+	txUDPPort = get_node("txUDPPort")
+	btnStartServer = get_node("btnStartServer")
+	btnStartClient = get_node("btnStartClient")
+	btnDisconnect = get_node("btnDisconnect")
+	output = get_node("output")
+	input = get_node("input")
+	
+	oConnection = Connection.new(txIP.get_text())
+	# Add the node to the scene tree, so it can _process
+	add_child(oConnection)
+	#Register the signals for client handling
+	oConnection.connect("onConnect", self, "onConnectAsClient")
+	oConnection.connect("onDisconnect", self, "onDisconnect")
+	#Register the signals for server handling
+	oConnection.connect("onClientConnect", self, "onClientConnect")
+	oConnection.connect("onClientDisconnect", self, "onClientDisconnect")
+	#On receiving messages
+	oConnection.connect("onReceive", self, "onReceive")
+
+func handleDisconnect():
+	btnStartServer.set_disabled(false)
+	btnStartClient.set_disabled(false)
+	btnDisconnect.set_disabled(true)
+	output.add_text( "Connection Closed "); output.newline()
+
+func setConnectionInfo():
+	oConnection.setIP(txIP.get_text())
+	oConnection.setTCPPort(txPort.get_text())
+	oConnection.setUDPPort(txUDPPort.get_text())
+
+func _on_btnStartServer_pressed():
+	setConnectionInfo()
+	if oConnection.start_server():
+		btnStartServer.set_disabled(true)
+		btnStartClient.set_disabled(true)
+		btnDisconnect.set_disabled(false)
+		output.add_text( "Server started at port "+str(oConnection.tcpPort)); output.newline()
+	else:
+		output.add_text( "Error starting server, maybe the por is already in use."); output.newline()
+
+func _on_btnStartClient_pressed():
+	setConnectionInfo()
+	output.add_text( "Starting as client on "+oConnection.ip+":"+str(oConnection.tcpPort)); output.newline()
+	btnStartServer.set_disabled(true)
+	btnStartClient.set_disabled(true)
+	btnDisconnect.set_disabled(false)
+	if not oConnection.start_client():
+		output.add_text( "Error starting client on "+oConnection.ip+":"+str(oConnection.tcpPort)); output.newline()
+		handleDisconnect()
+
+func _on_btnDisconnect_pressed():
+	oConnection.disconnect()
+	handleDisconnect()
+
+func _on_input_input_event( ev ):
+	if ev.is_action("message_send") and not input.get_text().empty():
+		if oConnection.sendData(input.get_text()):
+			output.add_text( "Sent > "+input.get_text()); output.newline()
+		input.set_text("")
+
+func onConnectAsClient(ip, port):
+	output.add_text( "Connected on "+ip+":"+str(port)); output.newline()
+
+func onDisconnect(ip, port):
+	output.add_text( "Disconnected from "+ip+":"+str(port)); output.newline()
+	handleDisconnect()
+
+func onClientConnect(oClient):
+	output.add_text( "Client "+oClient.ip+" has connected on server."); output.newline()
+
+func onClientDisconnect(oClient):
+	output.add_text( "Client "+oClient.ip+" has disconnected from server."); output.newline()
+
+func onReceive(data, sender):
+	if not sender.isHost:
+		output.add_text( "From "+sender.ip+" > "+data); output.newline()
+	else:
+		output.add_text( "Server > "+data); output.newline()
+
+
+
+
